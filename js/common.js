5418ea3a4aa73c8b6b4e070bdd7fa8c1d8124352
minho5814 <tkmino84@gmail.com>
2017-02-08 14:12:12 +0900

    스크립트 공백 정리

--- js/common.js
+++ js/common.js
@@ -10,20 +10,20 @@ $(window).load(function(){
 		anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
 
 		onLeave: function(anchorLink, index){
-			$('.section').find('.h1-logo, .text-area, .quotation-box').stop().animate({'margin':'0 0 0 -130%', 'opacity':'0'},400);
-			$('.section .title-box').stop().animate({'margin':'0 -130% 0 0', 'opacity':'0'},400);
-			$('.section .slide .inner .center-text').stop().animate({'margin':'0 0 -150% 0','opacity':'0'},400,function(){
+			$('.section').find('.h1-logo, .text-area, .quotation-box').stop().animate({'margin':'0 0 0 -130%', 'opacity':'0'}, 400);
+			$('.section .title-box').stop().animate({'margin':'0 -130% 0 0', 'opacity':'0'}, 400);
+			$('.section .slide .inner .center-text').stop().animate({'margin':'0 0 -150% 0', 'opacity':'0'}, 400, function(){
 				$('.section').find('.h1-logo, .text-area, .quotation-box, .title-box').css({'margin':'-130% 0 0 0'});
-				$('.section .slide .inner .center-text').css({'margin':'-150% 0 0 0'})
+				$('.section .slide .inner .center-text').css({'margin':'-150% 0 0 0'});
 			});
 		},
 		afterLoad: function(anchorLink, index){
 			//section 1
 			if(index == 1){
 				$('#section0').find('.h1-logo, .text-area, .quotation-box, .title-box').stop().animate({'margin':'0', 'opacity':'1'}, 800);
-				$('#section0 .slide .inner .center-text').delay(100).stop().animate({'margin':'20px 0 0 0','opacity':'1'},800,function(){
-					$(this).stop().animate({'margin':'-20px 0 0 0'},200,function(){
-						$(this).stop().animate({'margin':'0'},200);
+				$('#section0 .slide .inner .center-text').delay(100).stop().animate({'margin':'20px 0 0 0', 'opacity':'1'}, 800, function(){
+					$(this).stop().animate({'margin':'-20px 0 0 0'}, 200, function(){
+						$(this).stop().animate({'margin':'0'}, 200);
 					});
 				});
 				$('.nav2 .nav-item:first-child a').click();
@@ -33,9 +33,9 @@ $(window).load(function(){
 			//section 2
 			if(index == 2){
 				$('#section1').find('.h1-logo, .text-area, .quotation-box, .title-box').stop().animate({'margin':'0', 'opacity':'1'}, 800);
-				$('#section1 .slide .inner .center-text').delay(100).stop().animate({'margin':'20px 0 0 0','opacity':'1'},800,function(){
-					$(this).stop().animate({'margin':'-20px 0 0 0'},200,function(){
-						$(this).stop().animate({'margin':'0'},200);
+				$('#section1 .slide .inner .center-text').delay(100).stop().animate({'margin':'20px 0 0 0', 'opacity':'1'}, 800, function(){
+					$(this).stop().animate({'margin':'-20px 0 0 0'}, 200, function(){
+						$(this).stop().animate({'margin':'0'}, 200);
 					});
 				});
 				$('.nav1 .nav-item:first-child a').click();
@@ -45,9 +45,9 @@ $(window).load(function(){
 			//section 1
 			if(index == 3){
 				$('#section2').find('.h1-logo, .text-area, .quotation-box, .title-box').stop().animate({'margin':'0', 'opacity':'1'}, 800);
-				$('#section2 .slide .inner .center-text').delay(100).stop().animate({'margin':'20px 0 0 0','opacity':'1'},800,function(){
-					$(this).stop().animate({'margin':'-20px 0 0 0'},200,function(){
-						$(this).stop().animate({'margin':'0'},200);
+				$('#section2 .slide .inner .center-text').delay(100).stop().animate({'margin':'20px 0 0 0', 'opacity':'1'}, 800, function(){
+					$(this).stop().animate({'margin':'-20px 0 0 0'}, 200, function(){
+						$(this).stop().animate({'margin':'0'}, 200);
 					});
 				});
 				$('.nav1 .nav-item:first-child a').click();
@@ -63,9 +63,9 @@ $(window).load(function(){
 		$('.section').eq(bIdx).find('.fp-slidesNav li').eq(iIdx).find('a').click();
 	});
 	$('.section').find('.h1-logo, .text-area, .quotation-box, .title-box').animate({'margin':'0', 'opacity':'1'}, 800);
-	$('.section .slide .inner .center-text').delay(100).animate({'margin':'20px 0 0 0','opacity':'1'},800,function(){
-		$(this).animate({'margin':'-20px 0 0 0'},200,function(){
-			$(this).animate({'margin':'0'},200);
+	$('.section .slide .inner .center-text').delay(100).animate({'margin':'20px 0 0 0', 'opacity':'1'}, 800, function(){
+		$(this).animate({'margin':'-20px 0 0 0'}, 200, function(){
+			$(this).animate({'margin':'0'}, 200);
 		});
 	});
 
